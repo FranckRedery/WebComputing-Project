702fd99de545513f43b09f055153399332733255
Fiabio97 <giacomino@fabio.lan>
2021-12-17 17:00:45 +0100

    Configurazione

---
+++ src/main/java/techPlanet/TechPlanetApplication.java
@@ -0,0 +1,13 @@
+package techPlanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechPlanetApplication.class, args);
+	}
+
+}
