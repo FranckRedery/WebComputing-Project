cfd7425755fc790e5578f0cdc0116b94eeecb0bc
Fiabio97 <giacomino@fabio.lan>
2021-12-19 23:02:37 +0100

    db

---
+++ src/main/java/techPlanet/Database.java
@@ -0,0 +1,30 @@
+package techPlanet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Database {
+	
+	private static Database instance = null;
+	private Connection conn;
+	
+	public static Database getInstance() {
+		if (instance == null) {
+			instance = new Database();
+		}
+		return instance;
+	}
+	
+	private Database() {
+		try {
+			conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techplanet", 
+											"postgres", "postgres");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
