7494bf3e457a42c87305843eb75a82a39a457719
Francesco <Francesco@DESKTOP-IMKJN3E>
2022-01-17 17:35:13 +0100

    bug fix ora worka

--- src/main/resources/static/js/itemReturn/itemReturn.js
+++ src/main/resources/static/js/itemReturn/itemReturn.js
@@ -21,38 +21,31 @@
             event.stopPropagation()
           }
   		  else{
-	        alert("The return request has been created. You will be able to view the status of the return on your personal returns page");
-			createRequest();
+			alert("The return request has been created. You will be able to view the status of the return on your personal returns page")
+			let id = document.getElementById('prodId').innerHTML;
+
+			let product = new Product(id);
+			let quantity = document.getElementById('qty').innerHTML;
+			let reason = document.getElementById('validationReason').value;
+			let description = document.getElementById('textArea').value;
+	
+			let returnRequest = new ReturnRequest(product,reason,description,quantity);
+	
+			$.ajax({
+				type: "POST",
+				url: "/addRequest", 
+				contentType: "application/json",
+				data: JSON.stringify(returnRequest),
+				async:false,
+				success: function(){ window.location.href = "myOrder.html"}
+			});
+			event.preventDefault();
 		  }
           form.classList.add('was-validated')
         }, false)
       })
   })()
 
-
-
-
-function createRequest(){
-	
-
-	let id = document.getElementById('prodId').innerHTML;
-
-	let product = new Product(id);
-	let quantity = document.getElementById('qty').innerHTML;
-	let reason = document.getElementById('validationReason').value;
-	let description = document.getElementById('textArea').value;
-	
-	let returnRequest = new ReturnRequest(product,reason,description,quantity);
-	
-	$.ajax({
-			type: "POST",
-			url: "/addRequest",
-			contentType: "application/json",
-			data: JSON.stringify(returnRequest),
-			success: function(){ window.location.href = "/myOrder.html";}	
-			});
-	
-}
 
 
 
